498c182db1de540d1cb9d343eac0cbf364e6d15e
Joel <joelwul@gmail.com>
2026-08-22 00:28:15 -0300

    actualizacion automatica

---
+++ SUBIR.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+title SUBIR cambios a Vercel
+cd /d "%~dp0"
+git add .
+git commit -m "actualizacion automatica"
+git push
+echo.
+echo LISTO! Vercel se redeploya solo en ~1 minuto.
+pause
